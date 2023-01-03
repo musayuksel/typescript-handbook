dc400b5462f61a7a40480e11bde8f65e5fe17dd5
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-03 15:22:02 +0000

    Literal types error handling

--- EverydayTypes/literalTypes/script.ts
+++ EverydayTypes/literalTypes/script.ts
@@ -7,3 +7,13 @@ interface Options {
   configure({ width: 100 });
   configure("auto");
 //   configure("automatic");//error because "automatic" is not a member of the union type
+
+// MORE ADVANCED 
+declare function handleRequest(url: string, method: "GET" | "POST"): void;
+
+const req = { url: "https://example.com", method: "GET" };
+handleRequest(req.url, req.method);//Error because req.method is not a member of the union type
+
+//solution 1 // Type assertion
+handleRequest(req.url, req.method as "GET");//OR
+handleRequest(req.url, req.method as "GET" | "POST");
