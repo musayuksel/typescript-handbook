bf6e9177076a3b0d372ba9fe6925504259a917c9
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-03 14:42:07 +0000

    Working with union types

--- EverydayTypes/unionTypes/script.ts
+++ EverydayTypes/unionTypes/script.ts
@@ -5,3 +5,14 @@ function printId(id: number | string) {
 printId(101);
 printId('202');
 //   printId({ myID: 22342 });  // Error, object is not a number or string
+
+function printId2(id: number | string) {
+  // console.log('Your ID is: ' + id.toUpperCase()); //error because id is a union type
+  if (typeof id === 'string') {
+    // In this branch, id is of type 'string'
+    console.log(id.toUpperCase());
+  } else {
+    // Here, id is of type 'number'
+    console.log(id);
+  }
+}
