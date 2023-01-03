16812c131012ae6463ba06ceba9473d8de83af17
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-03 14:52:44 +0000

    Types and Interfaces

---
+++ EverydayTypes/typeAliases&Interfaces/script.ts
@@ -0,0 +1,25 @@
+type Point = {
+  x: number;
+  y: number;
+};
+
+// Exactly the same as the earlier example
+function printCoord(pt: Point) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
+printCoord({ x: 100, y: 100 });
+
+// INTERFACE
+interface PointInterface {
+  x: number;
+  y: number;
+}
+
+// Exactly the same as the earlier example
+function printCoordInterface(pt: PointInterface) {
+  console.log("The coordinate's x value is " + pt.x);
+  console.log("The coordinate's y value is " + pt.y);
+}
+
