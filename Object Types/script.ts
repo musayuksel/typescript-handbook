a639af550c9dabd14a151042866a58b29c8ee01f
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-05 10:35:14 +0000

    Optional properties

--- Object Types/script.ts
+++ Object Types/script.ts
@@ -20,3 +20,24 @@ type Person2 = {
 function greet3(person: Person2) {
   return 'Hello ' + person.name;
 }
+
+// PROPERTY MODIFIERS-----------------------------
+// 1-OPTIONAL PROPERTIES
+interface PaintOptions {
+  shape: 'circle' | 'square';
+  xPos?: number;
+  yPos?: number;
+}
+
+function paintShape(opts: PaintOptions) {
+  // ...do something...
+  //the best practice is handle the undefined case
+  const newP = opts.xPos === undefined ? 0 : opts.xPos;
+}
+//OR destructuring and default value
+function paintShapeBetter({ shape, xPos = 0, yPos = 0 }: PaintOptions) {
+  // ...do something...
+}
+paintShape({ shape: 'circle' }); // OK
+paintShape({ shape: 'square', xPos: 100 }); // OK
+paintShape({ shape: 'square', xPos: 100, yPos: 100 }); // OK
