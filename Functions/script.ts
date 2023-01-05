c61aff31e2db8a4d36080c7ab693c618e1fe5deb
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-05 09:57:56 +0000

    Rest arguments

--- Functions/script.ts
+++ Functions/script.ts
@@ -194,3 +194,14 @@ function multiply(n: number, ...m: number[]) {
 }
 // 'a' gets value [10, 20, 30, 40]
 const a = multiply(10, 1, 2, 3, 4);
+
+// REST ARGUMENTS-----------------------------
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+arr1.push(...arr2);
+
+// Inferred type is number[] -- "an array with zero or more numbers",
+// not specifically two numbers
+// const args = [8, 5];
+const args = [8, 5] as const;
+const angle = Math.atan2(...args);
