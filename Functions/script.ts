40f60d1ab8fb11ceb7808b02eeeaf8984440bf4c
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-05 09:53:05 +0000

    Rest parameters

--- Functions/script.ts
+++ Functions/script.ts
@@ -186,3 +186,11 @@ function doSomething(f: Function) {
   //DON'T USE THIS, Return type is ANY
   return f(1, 2, 3);
 }
+
+// REST PARAMETERS-----------------------------
+// Rest parameters are a way to represent an indefinite number of arguments as an array.
+function multiply(n: number, ...m: number[]) {
+  return m.map((x) => n * x);
+}
+// 'a' gets value [10, 20, 30, 40]
+const a = multiply(10, 1, 2, 3, 4);
