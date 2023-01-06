c33fc9086e59cd4e98aa2dd2a19769f8d825aca4
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-06 12:59:13 +0000

    implement clauses

---
+++ CLASSES/classHeritage.ts
@@ -0,0 +1,26 @@
+// Like other languages with object-oriented features, classes in JavaScript can inherit from base classes.
+// implement CLAUSES -----------------------------
+interface Pingable {
+  ping(): void;
+}
+class Sonar implements Pingable {
+  ping() {
+    console.log('ping!'); //OK
+  }
+}
+
+// class Ball implements Pingable {//ERROR, Ball does not implement ping()
+//   pongPong() {
+//     console.log('pong!');
+//   }
+// }
+
+interface A {
+  x: number;
+  y?: number;
+}
+class C2 implements A {
+  x = 0;
+}
+const c = new C2();
+// c.y = 10;//Error implementing an interface with an optional property doesn’t create that property:
