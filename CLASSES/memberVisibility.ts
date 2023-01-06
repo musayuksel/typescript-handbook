69405b2843d884fdc6a5f44120391752ad368f1b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-06 13:28:07 +0000

    protected

--- CLASSES/memberVisibility.ts
+++ CLASSES/memberVisibility.ts
@@ -9,3 +9,26 @@ class PublicGreeter {
     console.log(`Hello, ${this.name}`);
   }
 }
+
+// 2- protected -------------------------------------
+// members are only visible to subclasses of the class they’re declared in.
+class ProtectedGreeter {
+  protected name: string;
+  constructor(name: string) {
+    this.name = name;
+  }
+  protected getName() {
+    return this.name;
+  }
+}
+class SpecialGreeter extends ProtectedGreeter {
+  constructor(name: string) {
+    super(name); //I can access the protected property name
+  }
+  greet() {
+    console.log(`Hello, ${this.getName()}`); //I can access the protected method getName()
+  }
+}
+const specialGreeter = new SpecialGreeter('Max');
+specialGreeter.greet(); //OK
+// specialGreeter.getName(); //Error, I can't access the protected method getName()
