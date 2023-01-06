c3856fe6dc412848ae91aaae3da2ee537d173a1b
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-06 10:29:41 +0000

    Template literal types

---
+++ Creating Types From Types/templateLiteralTypes.ts
@@ -0,0 +1,12 @@
+type World = 'world';
+type Greeting = `hello ${World}`; //type Greeting is "hello world"
+
+// ADDING NEW PROPERTIES TO EXISTING TYPES-----------------------------
+type EmailLocaleIDs = 'welcome_email' | 'email_heading';
+type FooterLocaleIDs = 'footer_title' | 'footer_sendoff';
+
+type AllLocaleIDs = `${EmailLocaleIDs | FooterLocaleIDs}_id`;
+
+// For each interpolated position in the template literal, the unions are cross multiplied:
+type Lang = 'en' | 'ja' | 'pt';
+type LocaleMessageIDs = `${Lang}_${AllLocaleIDs}`; //HUGE UNION
