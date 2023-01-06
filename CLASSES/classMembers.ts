a736dba70dae71c1562520990670efb099c5be82
Musa <79163385+musayuksel@users.noreply.github.com>
2023-01-06 11:17:40 +0000

    Basic classes and Constructors

---
+++ CLASSES/classMembers.ts
@@ -0,0 +1,23 @@
+// TypeScript offers full support for the class keyword introduced in ES2015.
+// A field declaration creates a PUBLIC writeable property on a class:
+class Person {
+  //   name: string; //error, we can change the strictPropertyInitialization to false in tsconfig.json
+}
+
+const person = new Person();
+person.name = 'Maximilian';
+
+// Constructor Functions
+class GoodPerson {
+  fistName: string;
+  lastName: string;
+  //   constructor(fistName: string, lastName: string) {
+  //     this.fistName = fistName;
+  //     this.lastName = lastName;
+  //   }
+  constructor(fistName = 'userName', lastName = 'userLastName') {
+    //default values
+    this.fistName = fistName;
+    this.lastName = lastName;
+  }
+}
